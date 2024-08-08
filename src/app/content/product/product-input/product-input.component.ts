48cb1d9d09a228a2105336ab188cf5150a51a059
igor <igor.chernyshov6636@gmail.com>
2024-08-08 13:55:35 +1000

    Layers-images-Done

--- src/app/content/product/product-input/product-input.component.ts
+++ src/app/content/product/product-input/product-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ScrollAnimateDirective} from "../../../directives/scroll-animate.directive";
 
 @Component({
@@ -10,6 +10,30 @@ import {ScrollAnimateDirective} from "../../../directives/scroll-animate.directi
   templateUrl: './product-input.component.html',
   styleUrl: './product-input.component.scss'
 })
-export class ProductInputComponent {
+export class ProductInputComponent  implements OnInit {
+  texts: string[] = [
+    'AI search and analysis across over 200,000 Australian laws',
+    'Find the information you need quickly and easily',
+    'Find the information you need quickly and easily',
+    'Analyze laws with advanced AI technology'
+  ];
+  currentTextIndex = 0;
+  currentText = this.texts[this.currentTextIndex];
+  nextTextIndex = (this.currentTextIndex + 1) % this.texts.length;
+  nextText = this.texts[this.nextTextIndex];
+  showCurrentText = true;
 
+  ngOnInit() {
+    setInterval(() => {
+      this.showCurrentText = false;
+
+      setTimeout(() => {
+        this.currentTextIndex = (this.currentTextIndex + 1) % this.texts.length;
+        this.currentText = this.texts[this.currentTextIndex];
+        this.nextTextIndex = (this.currentTextIndex + 1) % this.texts.length;
+        this.nextText = this.texts[this.nextTextIndex];
+        this.showCurrentText = true;
+      }, 500); // Время должно соответствовать времени анимации
+    }, 5000);
+  }
 }
