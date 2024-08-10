6c0c8a281c9748531a3dc3b357f473431f2f6bba
igor <igor.chernyshov6636@gmail.com>
2024-08-11 00:45:07 +1000

    video-Done

--- src/app/content/hero/video-chat/video-chat.component.ts
+++ src/app/content/hero/video-chat/video-chat.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostBinding, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostBinding, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-video-chat',
@@ -7,15 +7,29 @@ import {Component, HostBinding, OnInit} from '@angular/core';
   templateUrl: './video-chat.component.html',
   styleUrl: './video-chat.component.scss'
 })
-export class VideoChatComponent implements OnInit {
+export class VideoChatComponent implements OnInit, AfterViewInit {
   @HostBinding('class.load') isLoading = false;
+  @ViewChild('videoPlayer') videoPlayer: ElementRef<HTMLVideoElement> | any;
 
   constructor() {
   }
 
   ngOnInit() {
+
     setTimeout(() => {
       this.isLoading = true;
     }, 10);
+
+  }
+
+  ngAfterViewInit() {
+    this.isLoading = true;
+    this.videoPlayer.nativeElement.muted = true; // Убедитесь, что видео отключено
+    this.videoPlayer.nativeElement.play().catch((error: any) => {
+      console.log('Autoplay was prevented:', error);
+      this.videoPlayer.nativeElement.addEventListener('click', () => {
+        this.videoPlayer.nativeElement.play();
+      });
+    });
   }
 }
