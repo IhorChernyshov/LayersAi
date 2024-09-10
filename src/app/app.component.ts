6a428e72e6c2de588ddaee257423b67de47aebef
igor <igor.chernyshov6636@gmail.com>
2024-09-10 15:18:14 +1000

    custom-cursor-better - done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,12 @@ import {HomeComponent} from "./content/home/home.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-// export class AppComponent  {
-export class AppComponent implements AfterViewInit {
+export class AppComponent  {
+/*export class AppComponent implements AfterViewInit {
 
   @ViewChild('cursorDot') cursorDot!: ElementRef;
   @ViewChild('cursorLine') cursorLine!: ElementRef;
   ngAfterViewInit(): void {
- /*   const cursorDot = document.querySelector('[data-cursor-dot]') as HTMLElement;
-    const cursorLine = document.querySelector('[data-cursor-line]') as HTMLElement;*/
-
     window.addEventListener('mousemove', (e: MouseEvent) => {
       const posX = e.clientX;
       const posY = e.clientY;
@@ -26,10 +23,10 @@ export class AppComponent implements AfterViewInit {
         this.cursorDot.nativeElement.style.left = `${posX}px`;
         this.cursorDot.nativeElement.style.top = `${posY}px`;
       }
-     /* if (cursorLine) {
+     /!* if (cursorLine) {
         cursorLine.style.left = `${posX}px`;
         cursorLine.style.top = `${posY}px`;
-      }*/
+      }*!/
 
       this.cursorLine.nativeElement.animate({
         left: `${posX}px`,
@@ -37,5 +34,5 @@ export class AppComponent implements AfterViewInit {
       }, {duration: 300, fill: 'forwards'})
 
     });
-  }
+  }*/
 }
