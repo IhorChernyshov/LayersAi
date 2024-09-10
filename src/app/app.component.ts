7483b1707f1044ebe4520671c71ab3600a188a90
igor <igor.chernyshov6636@gmail.com>
2024-09-10 15:11:33 +1000

    custom-cursor - done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HomeComponent} from "./content/home/home.component";
 
@@ -9,7 +9,31 @@ import {HomeComponent} from "./content/home/home.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'LayersAi';
+export class AppComponent  {
+/*export class AppComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    const cursorDot = document.querySelector('[data-cursor-dot]') as HTMLElement;
+    const cursorLine = document.querySelector('[data-cursor-line]') as HTMLElement;
+
+    window.addEventListener('mousemove', (e: MouseEvent) => {
+      const posX = e.clientX;
+      const posY = e.clientY;
+
+      if (cursorDot) {
+        cursorDot.style.left = `${posX}px`;
+        cursorDot.style.top = `${posY}px`;
+      }
+     /!* if (cursorLine) {
+        cursorLine.style.left = `${posX}px`;
+        cursorLine.style.top = `${posY}px`;
+      }*!/
+
+      cursorLine.animate({
+        left: `${posX}px`,
+        top: `${posY}px`
+      }, {duration: 300, fill: 'forwards'})
+
+    });
+  }*/
 }
